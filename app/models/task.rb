fb37e4d203a4a57b2e834308823341cabd33559e
Michał Zezyk <zezyk.michal@gmail.com>
2016-06-01 19:36:03 +0200

    algorithm implemented and failing tests covered

--- app/models/task.rb
+++ app/models/task.rb
@@ -30,13 +30,53 @@ class Task
     end
 
     def order(tasks)
-      tasks
+      queue = []
+      in_deg = {}
+      final_order = []
+
+      # Update inner degrees
+      tasks.each do |task|
+        in_deg[task] ||= 0
+        task.try(:next).each do |next_task|
+          raise 'self dependency' if task == next_task
+          in_deg[next_task] ||= 0
+          in_deg[next_task] += 1
+        end
+      end
+
+      # Init queue with zero-in-degree tasks
+      tasks.each do |task|
+        queue.push(task) if in_deg[task] == 0
+      end
+
+      # Proccess zer-in-degree tasks one by one
+      while queue.present?
+        task = queue.pop
+        task.try(:next).each do |next_task|
+          in_deg[next_task] -= 1
+          queue.push(next_task) if in_deg[task] == 0
+        end
+        final_order << task
+      end
+
+      raise 'circular dependency' if final_order.size < tasks.size
+
+      final_order.reverse
+    end
+
+    def print_collection(tasks)
+      output = ""
+      tasks.each do |task|
+        output << task.name
+      end
+      output
     end
 
     private
 
     def parse_line(line)
-      line = line.scan(/(\S+)\s*=>\s*(\S*)/)
+      line = line.scan(/^(\S{1})\s*=>\s*(\S{0,1})$/)
+      raise 'invalid format' unless line.present?
       line.first
     end
 
