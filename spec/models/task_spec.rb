bed67acb2a8fe72e3202d6e2d864c9fb128da006
Michał Zezyk <zezyk.michal@gmail.com>
2016-06-01 04:19:45 +0200

    tests improved

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -6,30 +6,37 @@ RSpec.describe Task, type: :model do
       string_to_parse = "A => B"
       expect{Task.create_from_string(string_to_parse)}.not_to raise_error
     end
+
     it 'passes when more lines' do
       string_to_parse = "A => B\nC => D"
       expect{Task.create_from_string(string_to_parse)}.not_to raise_error
     end
+
     it 'passes when correct format' do
       string_to_parse = "A => B"
       expect{Task.create_from_string(string_to_parse)}.not_to raise_error
     end
+
     it 'fails when invalid format' do
       string_to_parse = "A => B =>"
       expect{Task.create_from_string(string_to_parse)}.to raise_error('invalid format')
     end
+
     it 'passes when the second element missing' do
       string_to_parse = "A => "
       expect{Task.create_from_string(string_to_parse)}.not_to raise_error
     end
+
     it 'fails when the first element missing' do
       string_to_parse = " => A"
       expect{Task.create_from_string(string_to_parse)}.to raise_error('invalid format')
     end
+
     it 'fails when first element has more than one character' do
       string_to_parse = "AB => B"
       expect{Task.create_from_string(string_to_parse)}.to raise_error('invalid format')
     end
+
     it 'fails when second element has more than one character' do
       string_to_parse = "A => BA"
       expect{Task.create_from_string(string_to_parse)}.to raise_error('invalid format')
@@ -42,17 +49,48 @@ RSpec.describe Task, type: :model do
       collection = Task.create_from_string(string_to_parse)
       expect{Task.order(collection)}.to raise_error('self dependency')
     end
+
     it 'fails when there is a circular dependency' do
       string_to_parse = "A => \nB => C\nC => F\nD => A\nE => \nF => B"
       collection = Task.create_from_string(string_to_parse)
       expect{Task.order(collection)}.to raise_error('circular dependency')
     end
+
     it 'returns single job when one job given' do
       a = Task.new(name: "a")
       collection = [a]
-      expect{Task.order(collection)}.to be([a])
+      expect(Task.order(collection)).to eq([a])
     end
-    it 'returns proper sequence when some jobs, one dependency'
-    it 'return proper sequence when many jobs and many dependencies'
+
+    it 'returns proper sequence when some jobs, one dependency' do
+      a = Task.new
+      b = Task.new
+      c = Task.new
+      b.next << c
+      collection = [a,b,c]
+      ordered = Task.order(collection)
+
+      expect(ordered.index(b)).to be < ordered.index(c)
+    end
+
+    it 'return proper sequence when many jobs and many dependencies' do
+      a = Task.new
+      b = Task.new
+      c = Task.new
+      d = Task.new
+      e = Task.new
+      f = Task.new
+      b.next << c
+      c.next << f
+      d.next << a
+      e.next << b
+      collection = [a,b,c,d,e,f]
+      ordered = Task.order(collection)
+
+      expect(ordered.index(b)).to be < ordered.index(c)
+      expect(ordered.index(c)).to be < ordered.index(f)
+      expect(ordered.index(d)).to be < ordered.index(a)
+      expect(ordered.index(e)).to be < ordered.index(b)
+    end
   end
 end
